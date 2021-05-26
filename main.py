e197044c3042f8141077d50c9fd27855e73d9f88
salehua <saleh.ahmad@augmedix.com>
2021-05-26 20:24:33 +0600

    Considered input and output packets under loss.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import numpy
 import socket
 from netfilterqueue import NetfilterQueue
 
-ROUTE_TRAFFIC = "iptables -A INPUT -j NFQUEUE --queue-num 1"
-ROUTE_TRAFFIC_DEL = "iptables -D INPUT -j NFQUEUE --queue-num 1"
+ROUTE_TRAFFIC = "iptables -A OUTPUT -j NFQUEUE --queue-num 1;iptables -A INPUT -j NFQUEUE --queue-num 1"
+ROUTE_TRAFFIC_IN_DEL = "iptables -D INPUT -j NFQUEUE --queue-num 1"
+ROUTE_TRAFFIC_OUT_DEL = "iptables -D OUTPUT -j NFQUEUE --queue-num 1"
+USAGE = "usage:\n main.py packetloss [jitter]\n  *packetloss in %\n  *jitter in ms"
 choices = [True, False]
 probs = [1, 0]
 
@@ -19,21 +21,27 @@ def accept_or_not(pkt):
         pkt.accept()
     else:
         dropCount += 1
-        if dropCount == 200:
+        if dropCount == 100:
             print("")
             dropCount = 0
-        if dropCount%2 == 0:
+        if dropCount % 2 == 0:
             print "d",
         pkt.drop()
 
 
 def readParameters():
+    global USAGE
     if len(sys.argv) >= 2:
         if len(sys.argv) == 2:
             return float(sys.argv[1]), -1
-        return float(sys.argv[1]), float(sys.argv[2])
+        ploss = float(sys.argv[1])
+        jbuff = float(sys.argv[2])
+        if ploss < 1 or jbuff < 1:
+            print(USAGE)
+            exit(1)
+        return ploss, jbuff
     else:
-        print("usage:\n main.py packetloss [jitter]\n  *packetloss in %\n  *jitter in ms")
+        print(USAGE)
         exit(1)
 
 
@@ -60,7 +68,9 @@ nfqueue.bind(1, accept_or_not)
 s = socket.fromfd(nfqueue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
 
 # Delete all old routes that may exist
-while runCMD(ROUTE_TRAFFIC_DEL):
+while runCMD(ROUTE_TRAFFIC_IN_DEL):
+    pass
+while runCMD(ROUTE_TRAFFIC_OUT_DEL):
     pass
 try:
     print("Routing Traffic through the LinkConditioner.")
@@ -70,7 +80,8 @@ except KeyboardInterrupt:
     print('')
 
 print("Traffic Route Deleted.")
-runCMD(ROUTE_TRAFFIC_DEL)
+runCMD(ROUTE_TRAFFIC_IN_DEL)
+runCMD(ROUTE_TRAFFIC_OUT_DEL)
 s.close()
 nfqueue.unbind()
 print("LinkConditioner Stopped!")
